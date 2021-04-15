37c80bb20b5bdcb7d3c5ec694c80ca25dae0bb97
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-04-15 11:24:48 -0700

    favs are adding to db

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -1,18 +1,18 @@
 
 const router = require("express").Router();
-const { User } = require("../models/User");
+const User = require("../models/User");
 
 router.get("/:userid/favorites", (req, res) => {
     console.log(req.params.userid)
     res.json({message: req.params.userid})
 })
 
-router.put("/:userid/:favorite", async (req, res) => {
+router.post("/:userid/:favorite", async (req, res) => {
     const restaurant = req.params.favorite
     const userid = req.params.userid
-    const updatedUser = await User.find(
-        { _id: userid},
-        {$set: {favRestaurants: restaurant}})
+    const foundUser = await User.findById(userid)
+    foundUser.favRestaurants.push({name: restaurant})
+    const updatedUser = foundUser.save()
     res.status(200).json({updatedUser})
     // const addedRestaurant = await 
 })
--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ const UserSchema = new mongoose.Schema({
         value: String
     }],
     favRestaurants: [{
-        id: String,
+        // id: String,
         name: String
     }]
 }, {
