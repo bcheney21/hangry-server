99134fb69ee9c2fce3c77cbdce147157249e86a7
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-04-14 19:53:42 -0700

    working on adding to favs

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -7,4 +7,13 @@ router.get("/:userid/favorites", (req, res) => {
     res.json({message: req.params.userid})
 })
 
+router.put("/:userid/:favorite", async (req, res) => {
+    const restaurant = req.params.favorite
+    const userid = req.params.userid
+    const updatedUser = await User.find(
+        { _id: userid},
+        {$set: {favRestaurants: restaurant}})
+    res.status(200).json({updatedUser})
+    // const addedRestaurant = await 
+})
 module.exports = router
