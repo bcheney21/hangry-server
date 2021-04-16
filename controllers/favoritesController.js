0a4d6f79ab974e333da0b5106b6b5766aef3f0bc
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-04-16 11:18:11 -0700

    MVP hit

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -2,11 +2,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
-router.get("/:userid/favorites", (req, res) => {
-    console.log(req.params.userid)
-    res.json({message: req.params.userid})
-})
-
 router.post("/:userid/:favorite", async (req, res) => {
     const restaurant = req.params.favorite
     const userid = req.params.userid
